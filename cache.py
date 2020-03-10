a9c1e0746612440f8fd84fa00a499a393f5e99e5
BooYaOfficial <58788575+BooYaOfficial@users.noreply.github.com>
2020-03-10 13:04:34 +0300

    'from_cache_to_mp3'

---
+++ cache.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+
+path_cache = 'C:\\Users\\booya\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\nahd6ha2.default\\cache2\\entries'
+norm_path_cache = os.path.normpath(path_cache)
+path_target_dir = 'C:\\Users\\booya\\Desktop\\music'
+norm_path_target_dir = os.path.normpath(path_target_dir)
+
+print(norm_path_cache)
+
+for _, _, filenames in os.walk(norm_path_cache):
+    for filename in filenames:
+        full_path = os.path.join(norm_path_cache, filename)
+        if 11500 > os.path.getsize(full_path)/1024 > 6000:
+            print(f'{filename} - {os.path.getsize(full_path) / 1024} кб')
+            os.rename(full_path, full_path + '.mp3')
+            shutil.move(full_path + '.mp3', norm_path_target_dir)
+
+
+
